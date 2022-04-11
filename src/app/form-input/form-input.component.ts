f75998a20a75d5cdf86f9c43e38a23716af66764
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-04-11 18:13:44 +0300

    feat.add event emmiter to save new quote generated

--- src/app/form-input/form-input.component.ts
+++ src/app/form-input/form-input.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit ,Output, EventEmitter} from '@angular/core';
+import {Quote} from '../quote';
 @Component({
-  selector: 'app-form-input',
+  selector: 'app-forms',
   templateUrl: './form-input.component.html',
   styleUrls: ['./form-input.component.css']
 })
 export class FormInputComponent implements OnInit {
+  
+  newQuote = new Quote(0, "", "",0, 0, "");
+
+  @Output() addQuote=new EventEmitter<Quote>();
+
+  saveQuote(){
+        this.addQuote.emit(this.newQuote);
+        this.newQuote = new Quote(0, "", "",0, 0, "");
+  }  
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
-
+    
 }

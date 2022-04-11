4320e82e19b81af3cda782efd66a2b3d096bd73c
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-04-11 17:54:43 +0300

    file: add quote component and inject quote

---
+++ src/app/quote-details/quote-details.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
+@Component({
+  selector: 'app-details',
+  templateUrl: './quote-details.component.html',
+  styleUrls: ['./quote-details.component.css']
+})
+export class QuoteDetailsComponent implements OnInit {
+  @Input() quote!:Quote  ;
+  
+  @Output() isDelete = new EventEmitter<boolean>();
+  @Output() isPlus = new EventEmitter<boolean>();
+  @Output() isMinus = new EventEmitter<boolean>();
+  
+  quoteDelete(remove:boolean) {
+    this.isDelete.emit(remove);
+  }
+  votePlus(plus:boolean) {
+    this.isPlus.emit(plus);
+  }
+  voteMinus(minus:boolean) {
+    this.isMinus.emit(minus);
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
